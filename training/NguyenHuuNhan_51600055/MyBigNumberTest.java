94dd768850742ab3482af09418de98b0516b94c9
nguyenhuunhan23 <43365682+nguyenhuunhan23@users.noreply.github.com>
2018-12-08 22:15:45 +0700

    test unit for MyBigNumber

---
+++ training/NguyenHuuNhan_51600055/MyBigNumberTest.java
@@ -0,0 +1,30 @@
+package myjava.mybignumber;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyBigNumberTest {
+
+	@Test
+	void testSum_N_1() {
+		MyBigNumber myBigNumber = new MyBigNumber();
+		String kq = myBigNumber.sum("1234", "4321");
+		assertEquals("5555", kq);
+	}
+	
+	@Test
+	void testSum_N_2() {
+		MyBigNumber myBigNumber = new MyBigNumber();
+		String kq = myBigNumber.sum("1239", "4322");
+		assertEquals("5561", kq);
+	}
+	
+	@Test
+	void testSum_N_3() {
+		MyBigNumber myBigNumber = new MyBigNumber();
+		String kq = myBigNumber.sum(null, null);
+		assertEquals("0", kq);
+	}
+
+}
