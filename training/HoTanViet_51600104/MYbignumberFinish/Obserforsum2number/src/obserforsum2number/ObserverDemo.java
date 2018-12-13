b6c451fd9be798fef0213433deb0f053b13af0fb
51600104 <51600104@student.tdtu.edu.vn>
2018-12-14 04:21:33 +0700

    inside MYbignumberFinish, has a Final project for MybigNumber, 51600104

---
+++ training/HoTanViet_51600104/MYbignumberFinish/Obserforsum2number/src/obserforsum2number/ObserverDemo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obserforsum2number;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 51600104
+ */
+public class ObserverDemo {
+    public static void main( String[] args ) {
+        Subject sub = new Subject();
+        // Client configures the number and type of Observers
+        new  MyBigNumber(sub);
+        
+       String s1 = "999";
+       String s2 = "999";
+      
+            
+            sub.setState(s1,s2);
+    }
+}
