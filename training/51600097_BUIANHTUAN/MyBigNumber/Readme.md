9266f49fa01fc31a8b2d22088edc9853ffc9fcc8
Thinh Nguyen <thinhnguyen01165@gmail.com>
2018-12-21 22:41:15 +0700

    Update NguyenPHanQuocThinh Follow review

--- training/51600097_BUIANHTUAN/MyBigNumber/Readme.md
+++
@@ -1,9 +0,0 @@
-
-Đây là bài cộng 2 số dành cho học sinh tiểu học từ lớp 3 trở xuống,  với mong muốn cải thiện khả năng tính toán của các em nhỏ.
-
-bài gồm có 4 phần:
-
-1) phần code MyBigNumber.java
-2) interface GetData.java
-3) phần testcase mà mình đưa ra để ví dụ nhiều trường hợp các bạn có thể chạy thử, cách làm được gợi ý trong file PassThisProject.java
-4) phần GUI (chưa gắn code).
--- training/NguyenPhanQuocThinh_51600084/MyBigNumber/src/MyBigNumber.java
+++ training/NguyenPhanQuocThinh_51600084/MyBigNumber/src/MyBigNumber.java
@@ -2,16 +2,12 @@ import java.io.PrintStream;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-
 /**
  *
  * Tác Giả: Nguyễn Phan Quốc Thịnh
  * 
  */
-
 public class MyBigNumber {
-    
         /*example : 38 + 9
         Step 1: 8 cộng 9 bằng 17 nhớ 1
 	 3 + 0 + nho = 4
@@ -24,8 +20,7 @@ public class MyBigNumber {
         this.ireceiver = ireceiver;
     }
     
-    public String sum( String s_1, String s_2){
-        
+    public String sum(String s_1, String s_2){
         // Khai Báo ở đây
         
         //Pattern xét kí tự còn \D trong class partern có ký tự là số từ 0 đến 9
@@ -35,9 +30,6 @@ public class MyBigNumber {
         
         String result = "";
         String direction = "";  
-        
-        String c;
-        int id;
 
         int length1 = s_1.length();// do dai chuoi thu 1
         int length2 = s_2.length();// do dai chuoi thu 2
@@ -49,6 +41,9 @@ public class MyBigNumber {
         int tong = 0;// Khởi tạo biến tổng = 0 để cộng 2 kí tự cuối cùng lại với nhau
         int str_1 = s_1.length(); //lấy độ dài của số đầu tiên
         int str_2 = s_2.length(); //lấy độ dài của số 2
+                
+        String c;
+        int id;
         
         // lay do dau lon nhat giua a va b
         int max = str_1;
@@ -63,47 +58,48 @@ public class MyBigNumber {
             c = m.group();
             id = m.start();
             if ("-".equals(c) && id == 0) {
-                this.ireceiver.receive("Chương trình chưa hỗ trợ số âm cho học sinh lớp 3. Vui lòng nhập lại số A ");
-                throw new NumberFormatException("Chương trình chưa hỗ trợ số âm cho học sinh lớp 3. Vui lòng nhập lại số A "); 
+                this.ireceiver.receive("Chương trình chưa hỗ trợ số âm cho học sinh lớp 3. "
+                        + "Vui lòng nhập lại số A ");
+                throw new NumberFormatException("Chương trình chưa hỗ trợ số âm cho học sinh lớp 3."
+                        + " Vui lòng nhập lại số A "); 
                 // Nếu người dùng nhập số âm thì pro thông báo cho
             }
             this.ireceiver.receive("Bạn đã nhập nhầm kí tự vào sô A. Vui lòng check lại.");
             throw new NumberFormatException("Bạn đã nhập nhầm kí tự vào sô A. Vui lòng check lại."); 
-            // // Nếu người dùng nhập chữ hoặc kí tự thì pro thông báo cho  
+                // Nếu người dùng nhập chữ hoặc kí tự thì pro thông báo cho  
         }
         m = p.matcher(s_2);
         if (m.find()) {
             c = m.group();
             id = m.start();
             if ("-".equals(c) && id == 0) {
-                this.ireceiver.receive("Chương trình chưa hỗ trợ số âm cho học sinh lớp 3. Vui lòng nhập lại số B ");
-                throw new NumberFormatException("Chương trình chưa hỗ trợ số âm cho học sinh lớp 3. Vui lòng nhập lại số B"); 
+                this.ireceiver.receive("Chương trình chưa hỗ trợ số âm cho học sinh lớp 3."
+                        + " Vui lòng nhập lại số B ");
+                throw new NumberFormatException("Chương trình chưa hỗ trợ số âm cho học sinh lớp 3. "
+                        + "Vui lòng nhập lại số B"); 
                 // Nếu người dùng nhập số âm thì pro thông báo cho 
             }
             this.ireceiver.receive("Bạn đã nhập nhầm kí tự vào sô B. Vui lòng check lại.");
             throw new NumberFormatException("Bạn đã nhập nhầm kí tự vào sô B. Vui lòng check lại."); 
-                // // Nếu người dùng nhập chữ hoặc kí tự thì pro thông báo cho      
+                // Nếu người dùng nhập chữ hoặc kí tự thì pro thông báo cho      
         }
 
-        // Lặp từ 0 đến max lần
+        // Vòng lặp từ 0 đến max lần, cứ chạy tới khi nào nhớ hết số và hiển thị số nhớ
         
         for (int i = 0; i < max; i++) {
             pos1 = length1 - i - 1;
             // cập nhật lại vị trí chuỗi s1
             pos2 = length2 - i - 1;
             // cập nhật lại vị trí chuỗi s2
-
-            
             c1 = (pos1 > -1) ? s_1.charAt(pos1) : '0';
-
             c2 = (pos2 >= 0) ? s_2.charAt(pos2) : '0';
-
             tong = (c1 - '0') + (c2 - '0') + nho;
             // chuyển kí tự thành số xong cộng cho số nhớ
             result = (tong % 10) + result;
             // Lấy kết quả tổng ở trên chia lấy dư cho 10 và ghép vào chuỗi kết quả
             nho = tong / 10;// Cập nhật lại số nhớ
-            direction = " Bước " + (i + 1) + ":\n " + c1 + " + " + c2 + " = " + (tong - nho) + " + " + nho + " = "+ tong + " . Ta có " + tong % 10 + " nhớ " + nho + ".\n";
+            direction = " Bước " + (i + 1) + ":\n " + c1 + " + " + c2 + " = " + (tong - nho) + " + " + nho + " = "+ tong 
+                    + " . Ta có " + tong % 10 + " nhớ " + nho + ".\n";
             this.ireceiver.receive(direction);
         }
 
